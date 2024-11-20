dc82cd893da6701b03c8777fa6ac6bc4d5f05b01
brunoraro7 <brunogradella1@gmail.com>
2024-11-20 19:16:51 -0300

    Update Script-Financeiro.sql

--- Script-Financeiro.sql
+++ Script-Financeiro.sql
@@ -1,12 +1,18 @@
-create  database projeto;
+--criação banco de dados
+create  database financeiro;
 
+--Criação tabela conta bancaria
 CREATE TABLE Conta  
 (
     numero_agencia INT NOT NULL PRIMARY KEY,
     nome_agencia VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE despesa
+ALTER TABLE Conta
+ADD CONSTRAINT unique_nome_agencia UNIQUE (nome_agencia);
+
+--Criação tabela de contas a pagar
+CREATE TABLE contas_a_pagar
 (
     usuario_id INT NOT NULL PRIMARY KEY,
     valor       INT NOT null,
@@ -14,68 +20,63 @@ CREATE TABLE despesa
     forma_pagamento varchar(100) not null,
 );
 
-ALTER TABLE despesa
+ALTER TABLE contas_a_pagar
 DROP COLUMN Conta;
 
-ALTER TABLE despesa
-DROP FOREIGN KEY fk_Usuario_x_despesa;
+ALTER TABLE contas_a_pagar
+DROP FOREIGN KEY fk_Usuario_x_contas_a_pagar;
 
 
-ALTER TABLE despesa
+ALTER TABLE contas_a_pagar
 DROP COLUMN usuario_id;
 
 
-ALTER TABLE despesa
+ALTER TABLE contas_a_pagar
 ADD COLUMN pagador INT,
 ADD COLUMN recebedor INT;
 
-ALTER TABLE despesa
+ALTER TABLE contas_a_pagar
 ADD CONSTRAINT fk_pagador FOREIGN KEY (pagador) REFERENCES usuario(usuario_id),
 ADD CONSTRAINT fk_recebedor FOREIGN KEY (recebedor) REFERENCES usuario(usuario_id);
 
 
-ALTER TABLE Conta
-ADD CONSTRAINT unique_nome_agencia UNIQUE (nome_agencia);
-
-ALTER TABLE despesa
-ADD CONSTRAINT fk_conta_x_despesa FOREIGN KEY (nome_agencia)
+ALTER TABLE contas_a_pagar
+ADD CONSTRAINT fk_conta_x_contas_a_pagar FOREIGN KEY (nome_agencia)
 REFERENCES Conta (nome_agencia);
 
-ALTER TABLE despesa
-ADD CONSTRAINT fk_conta2_x_despesa FOREIGN KEY (numero_agencia)
+ALTER TABLE contas_a_pagar
+ADD CONSTRAINT fk_conta2_x_contas_a_pagar FOREIGN KEY (numero_agencia)
 REFERENCES Conta (numero_agencia);
 
-ALTER TABLE despesa
-ADD CONSTRAINT fk_conta_x_despesa FOREIGN KEY (numero_agencia)
+ALTER TABLE contas_a_pagar
+ADD CONSTRAINT fk_conta_x_contas_a_pagar FOREIGN KEY (numero_agencia)
 REFERENCES Conta (numero_agencia);
 
-ALTER TABLE despesa
+ALTER TABLE contas_a_pagar
 ADD COLUMN numero_agencia INT;
 
-
-
-ALTER TABLE despesa
-ADD CONSTRAINT fk_conta_x_despesa FOREIGN KEY (numero_agencia)
+ALTER TABLE contas_a_pagar
+ADD CONSTRAINT fk_conta_x_contas_a_pagar FOREIGN KEY (numero_agencia)
 REFERENCES Conta (numero_agencia);
 
-alter table despesa
-add constraint fk_Usuario_x_despesa foreign key(usuario_id)
+alter table contas_a_pagar
+add constraint fk_Usuario_x_contas_a_pagar foreign key(usuario_id)
 references usuario(usuario_id);
 
-alter table despesa
-add constraint fk_conta_x_despesa foreign key(nome_agencia)
+alter table contas_a_pagar
+add constraint fk_conta_x_contas_a_pagar foreign key(nome_agencia)
 references Conta(nome_agencia);
 
-alter table despesa 
+alter table contas_a_pagar
 add column status ENUM('Não paga', 'Paga') DEFAULT 'Não paga';
 
-alter table despesa 
+alter table contas_a_pagar
 add column data_vencimento date not null ;
 
-alter table despesa 
+alter table contas_a_pagar
 modify column data_pagamento date;
     
-
+--Criação tabela usuario
 CREATE TABLE usuario
 (
     usuario_id INT NOT NULL PRIMARY KEY,
